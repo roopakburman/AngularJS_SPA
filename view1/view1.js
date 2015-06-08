ca013c73472825b02c9c3d5475af862493709626
Roopak Burman <roopak.burman@gmail.com>
2015-06-08 19:20:08 -0400

    First Commit

---
+++ view1/view1.js
@@ -0,0 +1,45 @@
+'use strict';
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', View1Func);
+/*With $scope*/
+/* function View1Func($scope) {
+$scope.something = 'hey there!';
+ $scope.items = [{
+    name: 'Scuba Diving Kit',
+    id: 7297510
+  },{
+    name: 'Snorkel',
+    id: 0278916
+  },{
+    name: 'Wet Suit',
+    id: 2389017
+  },{
+    name: 'Beach Towel',
+    id: 1000983
+  }];
+} */
+/*With this keyword*/
+function View1Func() {
+this.something = 'hey there!';
+ this.items = [{
+    name: 'Scuba Diving Kit',
+    id: 7297510
+  },{
+    name: 'Snorkel',
+    id: 0278916
+  },{
+    name: 'Wet Suit',
+    id: 2389017
+  },{
+    name: 'Beach Towel',
+    id: 1000983
+  }];
+}
